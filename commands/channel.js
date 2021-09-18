c69b7e1f9c4a7a36b903c7eb3aa571f867cd422d
Jovee/Giga <joveegames@gmail.com>
2021-09-18 20:24:15 +0900

    Trying to give mod permissions. Non working commit.

--- commands/channel.js
+++ commands/channel.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Permissions } = require('discord.js');
+
 const { categories } = require('../resources/categories');
 
 const commandName = 'channel';
@@ -10,16 +12,18 @@ module.exports = {
         .setName(commandName)
         .setDescription(commandDescription),
     async execute(interaction) {
-        const username = interaction.user.username;
-
+        const user = interaction.user;
+        const username = user.username;
+        const userId = user.id;
+        
         interaction.guild.channels.create(username + '\'s channel', {
             type: 'text',
             topic: 'Bot set topic',
             parent: categories.BOTCREATEDCHANNELS,
             // permissionOverwrites: [
             //     {
-            //         id: everyoneRole.id,
-            //         deny: ['VIEW_CHANNEL'],
+            //         id: "881471452036079667",//userId,
+            //         allow: [Permissions.FLAGS.MANAGE_MESSAGES]
             //     },
             // ],
         })
